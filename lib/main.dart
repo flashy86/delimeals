143ae8be36220e803d4870e9674b752ef55b8328
flashy86 <annecharlotte.lucas2021@campus-eni.fr>
2021-12-14 22:33:03 +0100

    Creation dossiers + meal_item

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,7 +22,12 @@ class MyApp extends StatelessWidget {
                   fontSize: 20,
                   fontFamily: 'RobotoCondensed',
                   fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
+    //  home: CategoriesScreen(),
+    initialRoute: '/', //default is '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
+    /* *** METHODE POUR TRANSMETTRE DATAS PAR CONSTRUCTEUR
+      Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
-    );
+    ); */
+    // METHODE DE NAVIGATION PAR ROUTES NOMMEES ET TRANSMISSION DATAS PAR CONTROLLER
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id, 
+        'title': title,});
   }
 
   @override
